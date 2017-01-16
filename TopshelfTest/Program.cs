d6c769a0278b8bc20e1d83919ad7e549b4a40dec
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-01-16 13:52:57 +0300

    Sample controller added

--- TopshelfTest/Program.cs
+++ TopshelfTest/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace TopshelfTest
+﻿using System;
+using ITCC.Logging.Core;
+using ITCC.Logging.Core.Loggers;
+using ITCC.Logging.Windows.Loggers;
+using Topshelf;
+
+namespace TopshelfTest
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
+            InitLoggers();
+
+            HostFactory.Run(x =>                                 
+            {
+                x.Service<ServerController>(s =>                        
+                {
+                    s.ConstructUsing(name => new ServerController());     
+                    s.WhenStarted(controller => controller.Start());              
+                    s.WhenStopped(controller => controller.Stop());               
+                });
+                x.RunAsLocalSystem();      
+                                      
+                x.SetDescription("Sample Topshelf Host");        
+                x.SetDisplayName("Topshelf test");                       
+                x.SetServiceName("Topshelf test");                       
+            });
+        }
+
+        private static void InitLoggers()
+        {
+            Logger.Level = LogLevel.Trace;
+
+            if (Environment.UserInteractive)
+                Logger.RegisterReceiver(new ColouredConsoleLogger());
+
+            Logger.RegisterReceiver(new DebugLogger());
         }
     }
 }
---
+++ TopshelfTest/ServerController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using ITCC.HTTP.Common.Enums;
+using ITCC.HTTP.Server.Core;
+using ITCC.HTTP.Server.Enums;
+using ITCC.HTTP.SslConfigUtil.Core.Enums;
+using ITCC.Logging.Core;
+using Newtonsoft.Json;
+using Server = ITCC.HTTP.Server.Core.StaticServer<object>;
+using ServerConfiguration = ITCC.HTTP.Server.Core.HttpServerConfiguration<object>;
+
+namespace TopshelfTest
+{
+    internal class ServerController
+    {
+        #region public
+
+        public void Start()
+        {
+            var config = GetConfig();
+            var startStatus = Server.Start(config);
+            if (startStatus != ServerStartStatus.Ok)
+            {
+                Logger.LogEntry("START", LogLevel.Error, $"Error starting server: {startStatus}");
+                return;
+            }
+            Logger.LogEntry("START", LogLevel.Info, "Server started");
+        }
+
+        public void Stop()
+        {
+            Server.Stop();
+            Logger.LogEntry("START", LogLevel.Info, "Server stopped");
+        }
+        #endregion
+
+        #region private
+
+        private void RegisterHandlers()
+        {
+            Server.AddRequestProcessor(new RequestProcessor<object>
+            {
+                AuthorizationRequired = false,
+                Handler = (account, request) => Task.FromResult(new HandlerResult(HttpStatusCode.OK, "Hello world")),
+                Method = HttpMethod.Get,
+                SubUri = "hello"
+            });
+        }
+
+        private ServerConfiguration GetConfig() => new ServerConfiguration
+        {
+            Port = 8888,
+            Protocol = Protocol.Http,
+            AllowGeneratedCertificates = true,
+            CertificateBindType = BindType.SubjectName,
+            LogBodyReplacePatterns = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("(\"Token\":\")([\\w\\d]+)(\")", "$1REMOVED_FROM_LOG$3")
+            },
+            LogProhibitedQueryParams = new List<string> { "password" },
+            LogProhibitedHeaders = new List<string> { "Authorization" },
+            ServerName = "ITCC Test",
+            StatisticsEnabled = true,
+            SubjectName = "localhost",
+            BodyEncoders = new List<BodyEncoder>
+                {
+                    new BodyEncoder
+                    {
+                        AutoGzipCompression = true,
+                        ContentType = "application/xml",
+                        Encoding = Encoding.UTF8,
+                        Serializer = o =>
+                        {
+                            using (var stringWriter = new StringWriter())
+                            {
+                                using (var xmlWriter = XmlWriter.Create(stringWriter))
+                                {
+                                    var xmlSerializer = new XmlSerializer(o.GetType());
+                                    xmlSerializer.Serialize(xmlWriter, o);
+                                }
+                                return stringWriter.ToString();
+                            }
+                        },
+                        IsDefault = false
+                    },
+                    new BodyEncoder
+                    {
+                        AutoGzipCompression = true,
+                        ContentType = "application/json",
+                        Encoding = Encoding.UTF8,
+                        Serializer = o => JsonConvert.SerializeObject(o,
+                            new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Serialize}),
+                        IsDefault = true
+                    }
+                },
+            CriticalMemoryValue = 1024
+        };
+
+        #endregion
+    }
+}
